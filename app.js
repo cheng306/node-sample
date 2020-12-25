01a8b16411e64845ecf37176f9787c8182527dd9
Chun Hei Cheng <johnnycheng0306@gmail.com>
2020-12-24 23:57:16 -0500

    change port

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express();
 
 app.get("/", (req, res)=>{
   res.send("sample response");
-})
+});
 
 const port = process.env.port || 3000;
 app.listen(port, ()=>{
   console.log("for testing purpose");
-})
+});

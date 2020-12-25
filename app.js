bf8149b89faba35e51d364cfde655ce18aeb067b
Chun Hei Cheng <johnnycheng0306@gmail.com>
2020-12-24 23:33:02 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res)=>{
+  res.send("sample response");
+})
+
+const port = process.env.port || 3000;
+app.listen(3000, ()=>{
+  console.log("for testing purpose");
+})
